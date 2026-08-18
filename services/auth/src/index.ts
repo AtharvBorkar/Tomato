0648a43df123ccfad991240b0534dcb279dc19d6
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-08-19 03:03:39 +0530

    Modified index.ts

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,4 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+dotenv.config()
 
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Auth servics is running on port ${PORT}');
+
+});
