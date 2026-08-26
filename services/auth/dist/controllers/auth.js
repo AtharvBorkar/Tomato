73c1f467493a05c859ff3c5d93972165bdb2d2bd
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-08-26 23:27:38 +0530

    Updated auth.ts

--- services/auth/dist/controllers/auth.js
+++ services/auth/dist/controllers/auth.js
@@ -1,3 +1,20 @@
+import User from "../model/User.js";
+import jwt from 'jsonwebtoken';
 export const loginUser = async (req, res) => {
-    res.json(req.body);
+    try {
+        const { email, name, picture } = req.body;
+        let user = await User.findOne({ email });
+        if (!user) {
+            user = await User.create({
+                name,
+                email,
+                image: picture,
+            });
+        }
+        const token = jwt.sign({ user }, process.env.JWT_SEC, {
+            expiresIn: "15d",
+        });
+    }
+    catch (error) {
+    }
 };
--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,4 +1,22 @@
 import { Request, Response} from "express";
+import User from "../model/User.js";
+import jwt from 'jsonwebtoken';
 export const loginUser = async(req:Request, res:Response) => {
-    res.json(req.body);
+    try {
+        const {email, name, picture} = req.body
+        let user = await User.findOne({email})
+        if(!user){
+            user = await User.create({
+                name,
+                email,
+                image: picture,
+            });
+        }
+
+        const token = jwt.sign({user}, process.env.JWT_SEC as string,{
+            expiresIn: "15d",
+        });
+    } catch (error) {
+        
+    }
 };
