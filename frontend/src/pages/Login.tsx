2d65acdfcea0391c9f7eea54b6ce920ab3905c2b
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-08-28 21:37:29 +0530

    modified login.tcs

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { authService } from "../main"
 import toast from "react-hot-toast"
+import { GoogleLogin } from '@react-oauth/google';
 
 const Login = () => {
     const [loading, setLoading] = useState(false)
