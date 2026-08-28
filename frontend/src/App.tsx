8c8221d7295fb56d89a466c9ee2d47a69b7c3820
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-08-28 21:14:39 +0530

    Modified and added login and Home in  frontend/sec/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from './pages/login';
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
-  
+  return <>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+  </BrowserRouter>
+  </>
+
 };
 
 export default App
