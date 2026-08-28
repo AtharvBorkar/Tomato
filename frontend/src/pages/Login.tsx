8076e385285ba52af4a65f4ccc5322cf9a71f92b
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-08-28 21:21:33 +0530

    Added auth service in main.tcx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,19 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Login = () => {
+    const [loading, setLoading] = useState(false)
+    const navigte = useNavigate()
+
+    const responsegoogle = async(authResult: any) =>{
+        setLoading(true)
+        try {
+            const result = await axios.post(``)
+        } catch (error) {
+            
+        }
+    }
   return (
     <div>login</div>
   )
