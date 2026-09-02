c64ce3cdfab3ba168cd3a6896c52d5e5010dcaba
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-02 22:39:15 +0530

    modified google oauth

--- services/auth/dist/config/googleConfig.js
+++ services/auth/dist/config/googleConfig.js
@@ -3,4 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const oauth2client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "authmessage");
+export const oauth2client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:5173/login");
+console.log("Client ID:", GOOGLE_CLIENT_ID);
+console.log("Client Secret exists:", !!GOOGLE_CLIENT_SECRET);
