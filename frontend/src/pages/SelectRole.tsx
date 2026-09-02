eb4ad0bd8c7b4954cbc10b804383ba3651a53724
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-03 03:16:14 +0530

    modified SelectRoles

--- frontend/src/pages/SelectRole.tsx
+++ frontend/src/pages/SelectRole.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 
-
+type Role = "customer" | "rider" | "seller" | null
 const SelectRole = () => {
+    const [role, setRole] = useState<Role>(null)
   return (
     <div>SelectRole</div>
   )
