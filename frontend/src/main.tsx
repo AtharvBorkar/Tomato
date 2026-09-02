648659b9dea7465123607a1596690bfd9fc1ad54
AtharvBorkar <Atharvborkar2006@gmail.com>
2026-09-03 02:28:59 +0530

    modified Main.tsx and added app provide in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AppProvider } from './context/AppContext.tsx';
 
 export const authService = "http://localhost:5000";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="579422474099-1cmra25uj9qpidoea21annhsl41h2s53.apps.googleusercontent.com">
-    <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
